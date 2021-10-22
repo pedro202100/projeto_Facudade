f84025fd65215891d57990cb9ac9fee006322bba
Pedro Henrique <pedrohenriquew9@gmail.com>
2021-10-22 09:10:23 -0300

    menu atualizado

--- src/controleDeEstoque/Principal.java
+++ src/controleDeEstoque/Principal.java
@@ -1,24 +1,45 @@
 package controleDeEstoque;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Principal {
     public static void main(String[] args){
-        Produto pp = new Produto();
-        
-        
-        int opcao = Integer.parseInt(JOptionPane.showInputDialog(null,"digite 1 para adicionar um produto \n2 para remover o produto digite \n3 para vizualizar"));
-        if(opcao == 1){
-            pp.adicionar();
-            
-        }else if(opcao == 2){
-           pp.remover();
-            
-        }else if(opcao == 3){
-           pp.exibirTabela();
-            
-        }
-            
-        
-    }
+        Scanner sc = new Scanner(System.in);
+        Produto p = new Produto();
+        int opcao = 0;
+	do {
+		System.out
+				.println("\n\n### SCE - Sistema de Controle de Estoque###");
+		System.out.println("\n                  =========================");
+		System.out.println("                  |     1 - Adicionar         |");
+		System.out.println("                  |     2 - Remover      |");
+		System.out.println("                  |     3 - sair        |");
+		System.out.println("                  =========================\n");
+                
+		System.out.print("opção-->");
+                opcao = Integer.parseInt(sc.next()) ;
+		System.out.print("\n");
+		switch (opcao) {
+                    case 1:
+                        p.adicionar();
+                        p.exibirTabela();
+                        break;
+                    case 2:
+                        p.remover();
+                        p.exibirTabela();
+                    
+                    case 3:
+                        p.sair();
+                        break;
+                    case 0:
+                        break;
+                    default: System.out.println("Opção Inválida!");
+                }
+            //case3 , 4  , etc...
+            	} while (opcao != 3);
 }
+
+        
+}
+
--- src/controleDeEstoque/Produto.java
+++ src/controleDeEstoque/Produto.java
@@ -37,6 +37,10 @@ public class Produto {
     void remover() {
         
     }
+
+    void sair() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
     
 }
