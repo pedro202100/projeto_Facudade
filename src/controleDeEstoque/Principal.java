a3a370f4ecb7ae33ae18e4045c4f8944f4642bc1
Pedro Henrique <pedrohenriquew9@gmail.com>
2021-10-22 16:02:49 -0300

    atualizaçao adicionar produto incompleto

--- src/controleDeEstoque/Principal.java
+++ src/controleDeEstoque/Principal.java
@@ -1,5 +1,6 @@
 package controleDeEstoque;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
@@ -7,38 +8,40 @@ public class Principal {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Produto p = new Produto();
-        int opcao = 0;
-	do {
-		System.out
-				.println("\n\n### SCE - Sistema de Controle de Estoque###");
-		System.out.println("\n                  =========================");
-		System.out.println("                  |     1 - Adicionar         |");
-		System.out.println("                  |     2 - Remover      |");
-		System.out.println("                  |     3 - sair        |");
-		System.out.println("                  =========================\n");
-                
-		System.out.print("opção-->");
-                opcao = Integer.parseInt(sc.next()) ;
-		System.out.print("\n");
-		switch (opcao) {
-                    case 1:
-                        p.adicionar();
-                        p.exibirTabela();
-                        break;
-                    case 2:
-                        p.remover();
-                        p.exibirTabela();
-                    
-                    case 3:
-                        p.sair();
-                        break;
-                    case 0:
-                        break;
-                    default: System.out.println("Opção Inválida!");
-                }
-            //case3 , 4  , etc...
-            	} while (opcao != 3);
-}
+        boolean opcao = true;
+        int opcao2;
+        String opcao3;
+        
+	while (opcao == true){
+            p.exibirTabela();
+
+            System.out.println("1 - Adicionar | 2 - Remover | 3 - sair");               
+            System.out.print("opção-->");
+            opcao3 = sc.next();
+            opcao2 =  Integer.parseInt(opcao3);
+            
+            System.out.print("\n");
+
+            if(opcao2 == 1){
+                System.out.print("digite o nome do produto:");
+                String nome = sc.next();
+                System.out.print("digite o Valor do produto:");
+                double valor = Float.parseFloat(sc.next());
+                System.out.print("digite a Quantidade do produto:");
+                int qtd = Integer.parseInt(sc.next());
+                p.adicionar(nome,valor,qtd);
+            }else if(opcao2 == 2){
+                p.remover();
+                p.exibirTabela();
+            }else if(opcao2 == 3){
+                p.sair();
+                opcao = false;
+            }else{
+                System.out.print("opçao invalida!\n");
+            }              
+            
+        };
+    }
 
         
 }
--- src/controleDeEstoque/Produto.java
+++ src/controleDeEstoque/Produto.java
@@ -5,41 +5,69 @@
  */
 package controleDeEstoque;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author pedro
  */
 public class Produto {
-    int id;
+    int id = 0;
     String nome;
     double valor;
     int quantidade;
+    List<String> produtos = new ArrayList();
     
  Produto(){
+       
  
  }
  
  Produto(int id,String n,double v,int qtd){
+        
      this.id = id;
      nome = n;
      valor = v;
      quantidade = qtd;
+     
 }
 
-    void adicionar() {
+    String adicionar(String n,double v,int q) {
+        id = id + 1;
+        nome = n;
+        valor = v;
+        quantidade = q;
         
+        produtos.add(Integer.toString(id));
+        produtos.add(nome);
+        produtos.add(Integer.toString((int) valor));
+        produtos.add(Integer.toString(quantidade));
+        return "Adicionado"; 
     }
 
     void exibirTabela() {
+        System.out.println("||  ID  ||  NOME  ||  VALOR  ||  QUANTIDADE  ||");
+        System.out.println("-----------------------------------------------");
+        if(produtos.size() == 0){
+            System.out.print("");
+        }else {
+            int tamanho = produtos.size();
+            for(int x = 0; x<=tamanho; x++){
+                    System.out.println("||  "+produtos.get(x)+"   ||  "+produtos.get(x+1)+"  ||   "+produtos.get(x+2)+"   ||       "+produtos.get(x+3)+"      ||");
+            }
+        }
+        
         
     }
 
-    void remover() {
-        
+    String remover() {
+        return "removido";
     }
 
-    void sair() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    String sair() {
+        return "sair";
     }
     
     
